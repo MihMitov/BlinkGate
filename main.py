4bb57cbb9fda7f04f2d50b122c994fa13a5a8a6b
MihMitov <mihmitov@gmail.com>
2026-05-08 07:43:23 +0300

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 def P1_servo_movement():
-    pins.servo_write_pin(AnalogPin.P0, Servo_up)
-    basic.pause(100)
-    basic.pause(close_duration)
-    pins.servo_write_pin(AnalogPin.P0, 0)
-def Both_servo_movement():
-    pins.servo_write_pin(AnalogPin.P0, Servo_up)
-    pins.servo_write_pin(AnalogPin.P1, Servo_up)
-    basic.pause(100)
-    basic.pause(close_duration)
-    pins.servo_write_pin(AnalogPin.P0, 0)
     pins.servo_write_pin(AnalogPin.P1, 0)
+    basic.pause(servo_operation_time)
+    basic.pause(close_duration)
+    pins.servo_write_pin(AnalogPin.P1, Servo_Up_P1)
+    basic.pause(servo_operation_time)
+
+def on_bluetooth_connected():
+    basic.pause(2000)
+    basic.show_icon(IconNames.YES)
+    bluetooth.start_uart_service()
+bluetooth.on_bluetooth_connected(on_bluetooth_connected)
+
+def on_bluetooth_disconnected():
+    basic.pause(2000)
+    basic.show_icon(IconNames.NO)
+bluetooth.on_bluetooth_disconnected(on_bluetooth_disconnected)
+
+def on_button_pressed_ab():
+    global mode
+    mode = 2
+    basic.show_leds("""
+        . # # # .
+        . # . . #
+        . # # # #
+        . # . . #
+        . # # # .
+        """)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_a():
     global mode
@@ -21,35 +38,8 @@ def on_button_pressed_a():
         . . . . .
         . . . . .
         """)
-    pins.servo_write_pin(AnalogPin.P0, Servo_up)
-    basic.pause(100)
-    basic.pause(close_duration)
-    pins.servo_write_pin(AnalogPin.P0, 0)
+    P2_servo_movement()
 input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_button_pressed_ab():
-    global mode
-    mode = 0
-    basic.show_leds("""
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        """)
-    pins.servo_write_pin(AnalogPin.P0, Servo_up)
-    pins.servo_write_pin(AnalogPin.P1, Servo_up)
-    basic.pause(100)
-    basic.pause(close_duration)
-    pins.servo_write_pin(AnalogPin.P0, 0)
-    pins.servo_write_pin(AnalogPin.P1, 0)
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
-
-def P0_servo_movement():
-    pins.servo_write_pin(AnalogPin.P0, Servo_up)
-    basic.pause(100)
-    basic.pause(close_duration)
-    pins.servo_write_pin(AnalogPin.P0, 0)
 
 def on_button_pressed_b():
     global mode
@@ -61,14 +51,17 @@ def on_button_pressed_b():
         . . . . .
         . . . . .
         """)
-    pins.servo_write_pin(AnalogPin.P1, Servo_up)
-    basic.pause(100)
-    basic.pause(close_duration)
-    pins.servo_write_pin(AnalogPin.P1, 0)
+    P1_servo_movement()
 input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def play_tone():
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    basic.pause(500)
 
 def on_logo_pressed():
     global mode
+    mode = 1
     basic.show_leds("""
         # # # # #
         # # # # #
@@ -76,18 +69,28 @@ def on_logo_pressed():
         # # # # #
         # # # # #
         """)
-    mode = 1
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
-index2 = 0
-letters_index = 0
-text_index = 0
+def P2_servo_movement():
+    wuKong.set_servo_angle(wuKong.ServoTypeList._180, wuKong.ServoList.S6, 0)
+    basic.pause(servo_operation_time)
+    basic.pause(close_duration)
+    wuKong.set_servo_angle(wuKong.ServoTypeList._180, wuKong.ServoList.S6, Servo_Up_P2)
+    basic.pause(servo_operation_time)
+    basic.pause(open_time)
+current_mors_code = ""
 index1 = 0
 word = ""
 code = ""
 mode = 0
-Servo_up = 0
+open_time = 0
+servo_operation_time = 0
+Servo_Up_P1 = 0
+Servo_Up_P2 = 0
 close_duration = 0
+wuKong.set_light_mode(wuKong.LightMode.OFF)
 index = 0
 text_list: List[str] = []
 letters_lower = ["a",
@@ -115,7 +118,39 @@ letters_lower = ["a",
     "w",
     "x",
     "y",
-    "z"]
+    "z",
+    " ",
+    "а",
+    "б",
+    "в",
+    "г",
+    "д",
+    "е",
+    "ж",
+    "з",
+    "и",
+    "й",
+    "к",
+    "л",
+    "м",
+    "н",
+    "о",
+    "п",
+    "р",
+    "с",
+    "т",
+    "у",
+    "ф",
+    "х",
+    "ц",
+    "ч",
+    "ш",
+    "щ",
+    "ъ",
+    "ь",
+    "ю",
+    "я",
+]
 morse_list = [".-",
     "-...",
     "-.-.",
@@ -141,63 +176,95 @@ morse_list = [".-",
     ".--",
     "-..-",
     "-.--",
-    "--.."]
+    "--..",
+    " ",
+    ".-",
+    "-...",
+    ".--",
+    "--.",
+    "-..",
+    ".",
+    "...-",
+        "--..",
+        "..",
+        ".---",
+        "-.-",
+        ".-..",
+        "--",
+        "-.",
+        "---",
+        ".--.",
+        ".-.",
+        "...",
+        "-",
+        "..-",
+        "..-.",
+        "....",
+        "-.-.",
+        "---.",
+        "----",
+        "--.-",
+        "-.--",
+        "-..-",
+        "..--",
+        ".-.-",]
 serial.redirect_to_usb()
-close_duration = 2000
-Servo_up = 40
-pins.servo_write_pin(AnalogPin.P0, 0)
-pins.servo_write_pin(AnalogPin.P1, 0)
-Both_servo_movement()
+serial.set_rx_buffer_size(32)
+serial.set_tx_buffer_size(32)
+close_duration = 300
+Servo_Up_P2 = 37
+Servo_Up_P1 = 48
+servo_operation_time = 200
+pause_letters = 1100
+pause_words = 3100
+open_time = 0
+P1_servo_movement()
+P2_servo_movement()
+basic.pause(200)
 
 def on_forever():
-    global code, text_list, word, index1, text_index, letters_index, index2
-    while mode == 1:
-        pins.servo_write_pin(AnalogPin.P0, 0)
-        pins.servo_write_pin(AnalogPin.P1, 0)
-        serial.write_line("Waiting for word...")
+    global code, text_list, word, index1, current_mors_code
+    while mode == 2:
+        bluetooth.uart_write_line("Waiting for word...")
         code = ""
         text_list = []
-        music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
-            music.PlaybackMode.UNTIL_DONE)
-        basic.pause(500)
+        word = bluetooth.uart_read_until(serial.delimiters(Delimiters.NEW_LINE))
+        bluetooth.uart_write_line(word)
+        index1 = 0
+        text_list = word.split("")
+        for current_letter in text_list:
+            current_mors_code = morse_list[letters_lower.index_of(current_letter)]
+            for current_morse_char in current_mors_code:
+                if current_morse_char == ".":
+                    P2_servo_movement()
+                elif current_morse_char == "-":
+                    P1_servo_movement()
+                elif current_morse_char == " ":
+                    basic.pause(pause_words)
+                    basic.pause(pause_letters)
+            basic.pause(pause_letters)
+basic.forever(on_forever)
+
+def on_forever2():
+    global code, text_list, word, index1, current_mors_code
+    while mode == 1:
+        serial.write_line("Waiting for word...")
+        bluetooth.uart_write_line("Waiting for word...")
+        code = ""
+        text_list = []
         word = serial.read_line()
         serial.write_line(word)
-        music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
-            music.PlaybackMode.UNTIL_DONE)
-        basic.pause(500)
         index1 = 0
-        while index1 <= len(word) - 1:
-            text_list.append(word.char_at(index1))
-            index1 += 1
-        music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
-            music.PlaybackMode.UNTIL_DONE)
-        basic.pause(500)
-        if len(text_list) > 0:
-            text_index = 0
-            letters_index = 0
-            while len(text_list) > text_index:
-                if text_list[text_index] == letters_lower[letters_index]:
-                    code = "" + code + morse_list[letters_index] + "/"
-                    text_index += 1
-                    letters_index = 0
-                else:
-                    letters_index += 1
-            music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
-                music.PlaybackMode.UNTIL_DONE)
-            basic.pause(500)
-            index2 = 0
-            while index2 <= len(code) - 1:
-                if code.char_at(index2) == ".":
-                    P0_servo_movement()
-                elif code.char_at(index2) == "-":
+        text_list = word.split("")
+        for current_letter2 in text_list:
+            current_mors_code = morse_list[letters_lower.index_of(current_letter2)]
+            for current_morse_char2 in current_mors_code:
+                if current_morse_char2 == ".":
+                    P2_servo_movement()
+                elif current_morse_char2 == "-":
                     P1_servo_movement()
-                else:
-                    Both_servo_movement()
-                basic.pause(100)
-                index2 += 1
-        else:
-            basic.pause(3000)
-        music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
-            music.PlaybackMode.UNTIL_DONE)
-        basic.pause(500)
-basic.forever(on_forever)
+                elif current_morse_char2 == " ":
+                    basic.pause(pause_words)
+                    basic.pause(pause_letters)
+            basic.pause(pause_letters)
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -6,16 +6,13 @@ function P1_servo_movement () {
     basic.pause(servo_operation_time)
 }
 bluetooth.onBluetoothConnected(function () {
-    music.play(music.tonePlayable(784, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
-    music.play(music.tonePlayable(880, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
-    music.play(music.tonePlayable(988, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    basic.pause(2000)
+    basic.showIcon(IconNames.Yes)
     bluetooth.startUartService()
 })
 bluetooth.onBluetoothDisconnected(function () {
-    basic.pause(1000)
-    music.play(music.tonePlayable(165, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
-    music.play(music.tonePlayable(147, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
-    music.play(music.tonePlayable(131, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    basic.pause(2000)
+    basic.showIcon(IconNames.No)
 })
 input.onButtonPressed(Button.A, function () {
     mode = 0
@@ -33,16 +30,14 @@ function play_tone () {
     basic.pause(500)
 }
 input.onButtonPressed(Button.AB, function () {
-    mode = 0
+    mode = 2
     basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
+        . # # # .
+        . # . . #
+        . # # # #
+        . # . . #
+        . # # # .
         `)
-    P1_servo_movement()
-    P2_servo_movement()
 })
 input.onButtonPressed(Button.B, function () {
     mode = 0
@@ -55,7 +50,17 @@ input.onButtonPressed(Button.B, function () {
         `)
     P1_servo_movement()
 })
+function SpaceCheck () {
+    CharCodeList = []
+    for (let index = 0; index <= word.length - 1; index++) {
+        CharCodeList.push(convertToText(word.charCodeAt(index)))
+        if (word.charCodeAt(index) == 32) {
+            CharCodeList.push("32")
+        }
+    }
+}
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    mode = 1
     basic.showLeds(`
         # # # # #
         # # # # #
@@ -63,7 +68,7 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
         # # # # #
         # # # # #
         `)
-    mode = 1
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
 })
 function P2_servo_movement () {
     wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S6, 0)
@@ -71,47 +76,137 @@ function P2_servo_movement () {
     basic.pause(close_duration)
     wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S6, Servo_Up_P2)
     basic.pause(servo_operation_time)
+    basic.pause(open_time)
 }
 let current_mors_code = ""
 let index1 = 0
-let word = ""
+let text_list: string[] = []
 let code = ""
+let word = ""
+let CharCodeList: string[] = []
 let mode = 0
+let open_time = 0
 let servo_operation_time = 0
 let Servo_Up_P1 = 0
 let Servo_Up_P2 = 0
 let close_duration = 0
+let index2 = 0
 wuKong.setLightMode(wuKong.LightMode.OFF)
-let index = 0
-let text_list: string[] = []
+// a
+// b
+// c
+// d
+// e
+// f
+// g
+// h
+// i
+// j
+// k
+// l
+// m
+// n
+// o
+// p
+// q
+// r
+// s
+// t
+// u
+// v
+// w
+// x
+// y
+// z
+// space
+// а
+// б
+// в
+// г
+// д
+// е
+// ж
+// з
+// и
+// й
+// к
+// л
+// м
+// н
+// о
+// п
+// р
+// с
+// т
+// у
+// ф
+// х
+// ц
+// ч
+// ш
+// щ
+// ъ
+// ь (прескочил съм 203 - ы)
+// ю (прескочил съм 205 - э)
+// я
 let letters_lower = [
-"a",
-"b",
-"c",
-"d",
-"e",
-"f",
-"g",
-"h",
-"i",
-"j",
-"k",
-"l",
-"m",
-"n",
-"o",
-"p",
-"q",
-"r",
-"s",
-"t",
-"u",
-"v",
-"w",
-"x",
-"y",
-"z",
-" "
+"97",
+"98",
+"99",
+"100",
+"101",
+"102",
+"103",
+"104",
+"105",
+"106",
+"107",
+"108",
+"109",
+"110",
+"111",
+"112",
+"113",
+"114",
+"115",
+"116",
+"117",
+"118",
+"119",
+"120",
+"121",
+"122",
+"32",
+"176",
+"177",
+"178",
+"179",
+"180",
+"181",
+"182",
+"183",
+"184",
+"185",
+"186",
+"187",
+"188",
+"189",
+"190",
+"191",
+"128",
+"129",
+"130",
+"131",
+"132",
+"133",
+"134",
+"135",
+"136",
+"137",
+"138",
+"139",
+"142",
+"143"
 ]
 let morse_list = [
 ".-",
@@ -140,31 +235,80 @@ let morse_list = [
 "-..-",
 "-.--",
 "--..",
-" "
+" ",
+".-",
+"-...",
+".--",
+"--.",
+"-..",
+".",
+"...-",
+"--..",
+"..",
+".---",
+"-.-",
+".-..",
+"--",
+"-.",
+"---",
+".--.",
+".-.",
+"...",
+"-",
+"..-",
+"..-.",
+"....",
+"-.-.",
+"---.",
+"----",
+"--.-",
+"-.--",
+"-..-",
+"..--",
+".-.-"
 ]
 serial.redirectToUSB()
-serial.setRxBufferSize(32)
-serial.setTxBufferSize(32)
-close_duration = 150
+serial.setRxBufferSize(253)
+serial.setTxBufferSize(253)
+close_duration = 300
 Servo_Up_P2 = 37
-Servo_Up_P1 = 48
+Servo_Up_P1 = 46
 servo_operation_time = 200
 let pause_letters = 1100
 let pause_words = 3100
+open_time = 0
 P1_servo_movement()
 P2_servo_movement()
 basic.pause(200)
 basic.forever(function () {
-    while (mode == 1) {
-        serial.writeLine("Waiting for word...")
+    while (mode == 2) {
         bluetooth.uartWriteLine("Waiting for word...")
         code = ""
         text_list = []
         word = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
         bluetooth.uartWriteLine(word)
-        serial.writeLine(word)
+        SpaceCheck()
+        // Parsing the en or bg word
+        if (word.charCodeAt(0) == 208 || word.charCodeAt(0) == 209) {
+            for (let index22 = 0; index22 <= CharCodeList.length - 1; index22++) {
+                if (index22 % 2 != 0) {
+                    bluetooth.uartWriteLine(CharCodeList[index22])
+                    text_list.push(CharCodeList[index22])
+                }
+                if (index22 == CharCodeList.length - 2) {
+                    break;
+                }
+            }
+        } else {
+            for (let index3 = 0; index3 <= word.length - 1; index3++) {
+                bluetooth.uartWriteLine(convertToText(word.charCodeAt(index3)))
+                text_list.push(convertToText(word.charCodeAt(index3)))
+                if (index3 == word.length - 2) {
+                    break;
+                }
+            }
+        }
         index1 = 0
-        text_list = word.split("")
         for (let current_letter of text_list) {
             current_mors_code = morse_list[letters_lower.indexOf(current_letter)]
             for (let current_morse_char of current_mors_code) {
@@ -173,6 +317,32 @@ basic.forever(function () {
                 } else if (current_morse_char == "-") {
                     P1_servo_movement()
                 } else if (current_morse_char == " ") {
+                    basic.pause(pause_words)
+                    basic.pause(pause_letters)
+                }
+            }
+            basic.pause(pause_letters)
+        }
+    }
+})
+basic.forever(function () {
+    while (mode == 1) {
+        serial.writeLine("Waiting for word...")
+        bluetooth.uartWriteLine("Waiting for word...")
+        code = ""
+        text_list = []
+        word = serial.readLine()
+        serial.writeLine(word)
+        index1 = 0
+        text_list = word.split("")
+        for (let current_letter2 of text_list) {
+            current_mors_code = morse_list[letters_lower.indexOf(current_letter2)]
+            for (let current_morse_char2 of current_mors_code) {
+                if (current_morse_char2 == ".") {
+                    P2_servo_movement()
+                } else if (current_morse_char2 == "-") {
+                    P1_servo_movement()
+                } else if (current_morse_char2 == " ") {
                     basic.pause(pause_words)
                     basic.pause(pause_letters)
                 }
